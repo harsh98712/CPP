b6e71347c0aea2b04acce4972a3479c286b557e4
harsh98712 <harshbehl8837@gmail.com>
2026-07-09 12:41:31 +0530

    cpp

---
+++ 1_varconst/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int score;
+    score = 110;
+
+    const int uid = 232323;
+
+    int hiteshBalance = 500;
+    hiteshBalance = 1000;
+    
+    cout << "Welcome to chai with cpp 1" << endl ;    
+    cout << "Welcome to chai with cpp 2" << endl ;
+    cout << "Welcome to chai with cpp 3" << endl ;
+
+    return 0;
+}
