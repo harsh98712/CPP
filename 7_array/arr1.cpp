9cf240288e53e78d04bc6dfe5d3b991390dd6958
harsh98712 <harshbehl8837@gmail.com>
2026-07-13 13:16:51 +0530

    cpp

---
+++ 7_array/arr1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int chaitemperature[5] = {25,25,36,87};
+    cout <<"chaitemperature: ";
+   for(int i = 1;i < 5; i++){
+    cout <<chaitemperature[i]<<"degree c\n";
+      
+
+   }
+    return 0;
+}
